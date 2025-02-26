58b30c017dab064b59920d1fb33712c7195979e2
ElijahWood2003 <elijahdwood@gmail.com>
2025-02-26 13:55:17 -0500

    Fixed lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,20 +1,11 @@
-local lsp = require("lsp-zero")
+local lsp = require('lsp-zero')
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true}),
-	["<C-Space>"] = cmp.mapping.complete(),
-})
-
-lsp.set_preferences({
-	sign_icons = { }
-})
-
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(event)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({buffer = event.buf})
+  end
 })
 
 lsp.on_attach(function(client, bufnr)
