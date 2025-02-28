2fbf95cde1e2c6cd62c834f0bd23da7bc6bae55c
ElijahWood2003 <elijahdwood@gmail.com>
2025-02-28 13:19:41 -0500

    Fixed fugitive commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -15,7 +15,7 @@ local bufnr = vim.api.nvim_get_current_buf()
 local opts = {buffer = bufnr, remap = false}
 
 vim.keymap.set("n", "<leader>gp", function()
-	vim.cmd.Git('push')
+	vim.cmd('Git push origin')
 end, opts)
 
 vim.keymap.set("n", "<leader>gu", ":Git push -u origin", opts);
